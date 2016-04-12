2cb8e35dff454790396a9b9afac9b0ace363d6b7
Indraneel Pole <Indraneel.Pole@daenet.com>
2016-04-12 11:44:51 +0200

    Added solution to the project

---
+++ RasPiBrowser/MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RasPiBrowser
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            webView.Navigate(new Uri("http://google.com"));
+        }
+
+        private void Go_Web_Click(object sender, RoutedEventArgs e)
+        {
+            DoWebNavigate();
+        }
+
+
+        private void Web_Address_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                DoWebNavigate();
+            }
+        }
+
+        private void DoWebNavigate()
+        {
+            DismissMessage();
+
+            try
+            {
+                if (Web_Address.Text.Length > 0)
+                {
+                    webView.Navigate(new Uri("http://" + Web_Address.Text));
+                }
+                else
+                {
+                    DisplayMessage("You need to enter a web address.");
+                }
+            }
+            catch (Exception e)
+            {
+                DisplayMessage("Error: " + e.Message);
+            }
+        }
+
+        private void Go_FacebookClick(object sender, RoutedEventArgs e)
+        {
+            Web_Address.Text = "https://www.facebook.com";
+            DoWebNavigate();
+        }
+
+        private void Go_TwitterClick(object sender, RoutedEventArgs e)
+        {
+            Web_Address.Text = "https://www.twitter.com";
+            DoWebNavigate();
+        }
+
+        private void Go_YoutubeClick(object sender, RoutedEventArgs e)
+        {
+            Web_Address.Text = "https://www.youtube.com";
+            DoWebNavigate();
+        }
+
+        private void DisplayMessage(String message)
+        {
+            Message.Text = message;
+            MessageStackPanel.Visibility = Visibility.Visible;
+            webView.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void OnMessageDismiss_Click(object sender, RoutedEventArgs e)
+        {
+            DismissMessage();
+        }
+
+        private void DismissMessage()
+        {
+            webView.Visibility = Visibility.Visible;
+            MessageStackPanel.Visibility = Visibility.Collapsed;
+        }
+    }
+}
